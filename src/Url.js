1ccb17e6468549c1011121dea161094f8a531bd1
Tim Zhang <mrzhangtim@gmail.com>
2020-08-20 19:46:27 -0700

    reactTutorial

--- src/Url.js
+++ src/Url.js
@@ -8,7 +8,7 @@ class App extends Component {
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
     const url =
-      'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vRwAqp96T9sYYq2-i7Tj0pvTf6XVHjDSMIKBdZHXiCGGdNC0ypEU9NbngS8mxea55JuCFuua1MUeOj5/pubhtml#'
+    'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
 
     fetch(url)
       .then((result) => result.json())
